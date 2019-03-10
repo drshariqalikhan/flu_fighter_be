8c50229505e677a50519bc6659b9c1922fec4fd9
drshariqalikhan <speromedtech@gmail.com>
2019-03-10 13:36:09 +0800

    o

--- files/weather.py
+++ files/weather.py
@@ -4,11 +4,13 @@ from collections import defaultdict
 import time
 
 def getWeather(lat,lon):
-
+    n_url = f'http://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid=bed628740ac3ce2655a4dd92e21b1c73&cnt=8&units=metric'
     w_url = f"http://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid=bed628740ac3ce2655a4dd92e21b1c73&cnt=8&units=metric"
     q_url = f"https://api.waqi.info/feed/geo:{lat};{lon}/?token=ec80cc948e41a99f27d09e14b31d96bfdd93262e"
+    rn= requests.get(n_url)
     rw= requests.get(w_url)
     rq = requests.get(q_url)
+    json_data_n = json.loads(rn.text)
     json_data_w = json.loads(rw.text)
     json_data_q = json.loads(rq.text)
 
@@ -32,26 +34,11 @@ def getWeather(lat,lon):
     }
     airq = defaultdict(lambda: -1, airq)
 
-    now =[]
-
-    Timenow = int(time.time())
-    diff = 3600*2.5 
-    for w in weather_list:
-        ts = w['dt']
-        d = abs(Timenow - ts)
-        print(d)
-        if d <= diff:
-            now = w
-            break
-
-
-
-
 
 
     weatherData= {
         'forecast_list':weather_list,
-        'now':now,
+        'now':json_data_n,
         'city':city,
         'air_quality_now':airq
     }
